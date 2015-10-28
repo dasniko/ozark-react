b82d62ca82ba886ac9ae30ac76b739ef0bbeea7b
Niko Köbler <niko@n-k.de>
2015-10-28 14:38:25 -0700

    creating ThreadLocal the Java 8 way

--- src/main/java/dasniko/ozark/react/React.java
+++ src/main/java/dasniko/ozark/react/React.java
@@ -1,8 +1,8 @@
 package dasniko.ozark.react;
 
 import jdk.nashorn.api.scripting.NashornScriptEngine;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,21 +13,18 @@ import java.io.Reader;
  */
 public class React {
 
-    private ThreadLocal<NashornScriptEngine> engineHolder = new ThreadLocal<NashornScriptEngine>() {
-        @Override
-        protected NashornScriptEngine initialValue() {
-            NashornScriptEngine nashornScriptEngine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("nashorn");
-            try {
-                nashornScriptEngine.eval(read("nashorn-polyfill.js"));
-                nashornScriptEngine.eval(read("META-INF/resources/webjars/react/0.13.3/react.js"));
-                nashornScriptEngine.eval(read("META-INF/resources/webjars/showdown/0.3.1/src/showdown.js"));
-                nashornScriptEngine.eval(read("bookBox.js"));
-            } catch (ScriptException e) {
-                throw new RuntimeException(e);
-            }
-            return nashornScriptEngine;
+    private ThreadLocal<NashornScriptEngine> engineHolder = ThreadLocal.withInitial(() -> {
+        NashornScriptEngine nashorn = (NashornScriptEngine) new NashornScriptEngineFactory().getScriptEngine();
+        try {
+            nashorn.eval(read("nashorn-polyfill.js"));
+            nashorn.eval(read("META-INF/resources/webjars/react/0.13.3/react.js"));
+            nashorn.eval(read("META-INF/resources/webjars/showdown/0.3.1/src/showdown.js"));
+            nashorn.eval(read("bookBox.js"));
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
         }
-    };
+        return nashorn;
+    });
 
     public  String render(String function, Object object) {
         try {
