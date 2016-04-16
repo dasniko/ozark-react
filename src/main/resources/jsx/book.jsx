79679b64177b0e5d933d303dae15e21000c234ba
Niko Köbler <niko@n-k.de>
2016-04-16 20:09:00 +0200

    more materializecss

--- src/main/resources/jsx/book.jsx
+++ src/main/resources/jsx/book.jsx
@@ -4,9 +4,9 @@ export default class Book extends React.Component {
     render() {
         return (
             <div className="book">
-                <div className="divider"></div>
                 <h4>{this.props.author}</h4>
                 <div className="flow-text">{this.props.title}</div>
+                <div className="divider"></div>
             </div>
         );
     }
